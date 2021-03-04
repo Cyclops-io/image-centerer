8f170f390110a99afa382d68f9bafcf3d5d6b6a7
Christian Warren <clw@Isolation.local>
2021-03-04 08:17:16 -0500

    updated

--- centerer.py
+++ centerer.py
@@ -2,28 +2,15 @@
 
 import os, sys
 from PIL import Image
-
-def center(file):
-    inputImage = Image.open(file)
-    try:
-        inputImage.verify()
-    except Exception:
-        print('Invalid image')
-    wi = inputImage.size[1]
-    he = inputImage.size[0]
-    newWi = int(2*wi)
-    newHe = int(2*he)
-    newImage = Image.new('RGBA', (newWi, newHe),(255,255,255,0))
-    topcorH = int(he/2)
-    topcorW = int(wi/2)
-    newImage.paste(inputImage.copy(), (topcorH,topcorH))
-    newName = sys.argv[1].split(".")[0]
-    newName += '_Centered.png'
-    newImage.save(name, "PNG")
+from pathlib import Path
 
 if __name__ == "__main__":    
-    #center(str(sys.argv[1]))
-    inputImage = Image.open("bank.png")
+    file = Path(sys.argv[1])
+    #try:
+    #    inputImage.verify()
+    #except Exception:
+        #print('Invalid image')
+    inputImage = Image.open(file)
     wi = inputImage.size[1]
     he = inputImage.size[0]
     newWi = int(2*wi)
@@ -32,8 +19,6 @@ if __name__ == "__main__":
     topcorH = int(he/2)
     topcorW = int(wi/2)
     newImage.paste(inputImage.copy(), (topcorH,topcorH))
-    newName = sys.argv[1].split(".")[0]
-    newName += '_Centered.png'
-    newImage.save('centeredBank.png')
-
-    #print(f'centered {name} as {newName}')
+    output_filename = file.stem + '_centered' + file.suffix
+    newImage.save(output_filename)
+    print(f'centered {file} as {output_filename}')
